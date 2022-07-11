2e8d74a229639459cd6108006a0351ff5b2ddf40
yorkmacbook056 <saraemc@yahoo.com>
2022-07-11 10:34:41 -0500

    initial commit

---
+++ src/main/java/net/yorksolutions/sarabruwerprocessmanagerbackend/SaraBruwerProcessManagerBackEndApplication.java
@@ -0,0 +1,15 @@
+package net.yorksolutions.sarabruwerprocessmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaraBruwerProcessManagerBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaraBruwerProcessManagerBackEndApplication.class, args);
+    }
+
+}
+
+//this is a comment
---
+++ src/test/java/net/yorksolutions/sarabruwerprocessmanagerbackend/SaraBruwerProcessManagerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.sarabruwerprocessmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaraBruwerProcessManagerBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
